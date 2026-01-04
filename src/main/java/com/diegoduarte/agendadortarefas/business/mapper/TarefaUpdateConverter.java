6c0eb6fc989af6b1f8a97dc6871d8e4bc4add19c
Diego Duarte <diegocavalcante040307@gmail.com>
2026-01-04 17:26:42 -0300

    correcao de bug

--- src/main/java/com/diegoduarte/agendadortarefas/business/mapper/TarefaUpdateConverter.java
+++ src/main/java/com/diegoduarte/agendadortarefas/business/mapper/TarefaUpdateConverter.java
@@ -6,7 +6,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
 import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = "string", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface TarefaUpdateConverter {
 
     void updateTarefas (TarefasDTO tarefasDTO, @MappingTarget Tarefas tarefas);
